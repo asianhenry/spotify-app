70076583c8a4cbd56764c844440d274663c8ad50
asianhenry <henrybyoun@gmail.com>
2020-10-08 22:07:04 -0500

    updates

--- app.py
+++ app.py
@@ -1,90 +1,42 @@
-import json
-from flask import Flask, request, redirect, g, render_template
+from flask import Flask, request, abort, Response, redirect
+import os
 import requests
-from urllib.parse import quote
-from config import clinet_id, client_secret
-
-# Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
-# Visit this url to see all the steps, parameters, and expected response.
-
+import base64
+import json
 
 app = Flask(__name__)
 
-#  Client Keys
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+@app.route('/')
+def homepage():
 
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    CALLBACK_URL = os.environ.get('CALLBACK_URL')
+    REDIRECT_URL = os.environ.get('REDIRECT_URL')
 
-# Server-side Parameters
-CLIENT_SIDE_URL = "http://127.0.0.1"
-PORT = 8080
-REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
-SCOPE = "playlist-modify-public playlist-modify-private"
-STATE = ""
-SHOW_DIALOG_bool = True
-SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+    SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
-auth_query_parameters = {
-    "response_type": "code",
-    "redirect_uri": REDIRECT_URI,
-    "scope": SCOPE,
-    # "state": STATE,
-    # "show_dialog": SHOW_DIALOG_str,
-    "client_id": CLIENT_ID
-}
+    auth_token = request.args.get('code')
 
-
-@app.route("/")
-def index():
-    # Auth Step 1: Authorization
-    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
-    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
-    return redirect(auth_url)
-
-
-@app.route("/callback/q")
-def callback():
-    # Auth Step 4: Requests refresh and access tokens
-    auth_token = request.args['code']
     code_payload = {
         "grant_type": "authorization_code",
         "code": str(auth_token),
-        "redirect_uri": REDIRECT_URI,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
+        "redirect_uri": CALLBACK_URL
     }
-    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
 
-    # Auth Step 5: Tokens are Returned to Application
-    response_data = json.loads(post_request.text)
-    access_token = response_data["access_token"]
-    refresh_token = response_data["refresh_token"]
-    token_type = response_data["token_type"]
-    expires_in = response_data["expires_in"]
+    auth = "{}:{}".format(CLIENT_ID, CLIENT_SECRET)
+    base64encoded = base64.urlsafe_b64encode(auth.encode('UTF-8')).decode('ascii')
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
 
-    # Auth Step 6: Use the access token to access Spotify API
-    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+    response_data = json.loads(post_request.content)
 
-    # Get profile data
-    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
-    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
-    profile_data = json.loads(profile_response.text)
+    if 'error' in response_data:
+        abort(400)
+        abort(Response(response_data['error_description']))
 
-    # Get user playlist data
-    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
-    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
-    playlist_data = json.loads(playlists_response.text)
+    
+    return redirect('{redirect}/{access}'.format(redirect=REDIRECT_URL, access=response_data['access_token']), code=302)
 
-    # Combine profile and playlist data to display
-    display_arr = [profile_data] + playlist_data["items"]
-    return render_template("index.html", sorted_array=display_arr)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=PORT)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
